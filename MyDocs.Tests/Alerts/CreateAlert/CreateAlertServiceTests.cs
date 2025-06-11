604cb484e885731b8f0bde7d037389d419c98ef9
Adriano Vitor <84886079+AdriVitor@users.noreply.github.com>
2025-06-10 21:43:27 -0300

    Update path test project

---
+++ MyDocs.Tests/Alerts/CreateAlert/CreateAlertServiceTests.cs
@@ -0,0 +1,61 @@
+﻿using Moq;
+using MyDocs.Features.Alerts.CreateAlert;
+using MyDocs.Infraestructure.Services.ScheduleAlertService;
+using MyDocs.Models.Enums;
+using MyDocs.Models;
+using MyDocs.Tests.Shared;
+using MyDocs.Shared.DTOs;
+
+namespace MyDocs.Tests.Alerts.CreateAlert
+{
+    public class CreateAlertServiceTests
+    {
+        [Fact]
+        public async Task AddAlert_ShouldCreateAlertSuccessfully()
+        {
+            var userId = 1;
+            var email = "usuario@email.com";
+            var request = new CreateAlertRequest
+            {
+                IdUser = userId,
+                Name = "Alerta de Fatura",
+                Description = "Alerta mensal para pagamento",
+                RecurrenceOfSending = AlertRecurrence.Month
+            };
+
+            var expectedDate = DateTime.Now;
+
+            using var context = MemoryDatabase.Create();
+
+            context.UsersCredentials.Add(new UserCredentials
+            {
+                IdUser = userId,
+                Email = email,
+                Password = "Teste123"
+            });
+            context.SaveChanges();
+
+            var processAlertServiceMock = new Mock<IScheduleAlertService>();
+            processAlertServiceMock.Setup(x => x.ScheduleAlert(It.IsAny<ScheduleJobDTO>(), It.IsAny<EmailRequestDTO>()))
+                                   .Returns(Task.CompletedTask);
+
+            var service = new CreateAlertService(context, processAlertServiceMock.Object);
+
+            await service.AddAlert(request);
+
+            var createdAlert = context.Alerts.FirstOrDefault(a => a.IdUser == userId);
+
+            Assert.NotNull(createdAlert);
+            Assert.Equal(request.Name, createdAlert.Name);
+            Assert.Equal(request.Description, createdAlert.Description);
+            Assert.Equal((int)request.RecurrenceOfSending, createdAlert.RecurrenceOfSending);
+            Assert.Equal(userId, createdAlert.IdUser);
+            Assert.Equal(expectedDate.Date, createdAlert.CreationDate.Date);
+
+            processAlertServiceMock.Verify(p =>
+                p.ScheduleAlert(It.IsAny<ScheduleJobDTO>(), It.IsAny<EmailRequestDTO>()),
+                Times.Once);
+        }
+
+    }
+}
---
+++ MyDocs.Tests/Users/CreateUser/CreateUserServiceTests.cs
@@ -0,0 +1,49 @@
+﻿using Moq;
+using MyDocs.Features.Users.Create;
+using MyDocs.Infraestructure.ExternalServices.Email;
+using MyDocs.Tests.Shared;
+
+namespace MyDocs.Tests.Users.CreateUser
+{
+    public class CreateUserServiceTests
+    {
+        [Fact]
+        public async Task AddUser_ShouldCreateUserAndCredentialsAndSendEmail_WhenRequestIsValid()
+        {
+            using var context = MemoryDatabase.Create();
+
+            var emailServiceMock = new Mock<IEmailService>();
+
+            var service = new CreateUserService(context, emailServiceMock.Object);
+
+            var request = new CreateUserRequest
+            {
+                Name = "Test Create User",
+                CPF = "83590820063",
+                DateOfBirth = new DateTime(1995, 1, 1),
+                Phone = "11988991166",
+                Email = "testUser@email.com",
+                Password = "password123"
+            };
+
+            await service.AddUser(request);
+
+            var user = context.Users.FirstOrDefault(u => u.CPF == request.CPF);
+            Assert.NotNull(user);
+            Assert.Equal(request.Name, user.Name);
+            Assert.Equal(request.CPF, user.CPF);
+
+            var credentials = context.UsersCredentials.FirstOrDefault(c => c.IdUser == user.Id);
+            Assert.NotNull(credentials);
+            Assert.Equal(request.Email, credentials.Email);
+            Assert.Equal(request.Password, credentials.Password);
+            Assert.Equal(user.Id, credentials.IdUser);
+
+            emailServiceMock.Verify(e => e.SendEmail(
+                request.Email,
+                "Criação de conta MyDocs",
+                "<h1> Parabéns pela sua criação de conta no MyDocs </h1>"), Times.Once);
+        }
+
+    }
+}
