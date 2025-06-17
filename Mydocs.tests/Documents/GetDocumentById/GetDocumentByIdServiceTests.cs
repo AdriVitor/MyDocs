820656e6b6acaaa0ceb14ac7115ba6010a8218a5
Adriano Vitor <84886079+AdriVitor@users.noreply.github.com>
2025-06-16 21:50:54 -0300

    Add validator

---
+++ Mydocs.tests/Documents/GetDocumentById/GetDocumentByIdServiceTests.cs
@@ -0,0 +1,30 @@
+﻿using Moq;
+using MyDocs.Features.Documents.GetDocumentById;
+using MyDocs.Models;
+using MyDocs.Shared.Services.DocumentService;
+using MyDocs.Tests.Shared;
+using System.Security.Cryptography;
+
+namespace MyDocs.Tests.Documents.GetDocumentById
+{
+    public class GetDocumentByIdServiceTests
+    {
+
+        [Fact]
+        public async Task GetDocumentById_ShouldThrowException_WhenDocumentIsNull()
+        {
+            using var context = MemoryDatabase.Create();
+            var document = GenerateModelsService.CreateDocument(3, 3, "arquivo.pdf", ".pdf", 1024, "unique-name.pdf");
+            context.Documents.Add(document);
+            context.SaveChanges();
+
+            var request = new GetDocumentByIdRequest() { IdUser = 2, IdDocument = 2 };
+
+            var documentService = new Mock<IDocumentService>();
+
+            var service = new GetDocumentByIdService(documentService.Object);
+
+            await Assert.ThrowsAsync<ArgumentNullException>(() => service.GetDocumentById(request));
+        }
+    }
+}
